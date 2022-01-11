10944dbb253300b176aca668ddee04c173a9f41e
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-11 12:43:26 +0530

    Added:UC1:Modified

--- src/corejava/decOfCards.java
+++ src/corejava/decOfCards.java
@@ -12,8 +12,8 @@ import java.lang.Math;
 public class decOfCards {
     public static final String suit[]={"Clubs","Diamonds","Hearts","Spades"};
     public static final String rank[]={"2","3","4","5","6","7","8","9","10","Jack", "Queen", "King", "Ace"};
-    static Random  ra=new Random(52);
-    int [] Deck=new int[52];
+    static Random  ra=new Random();
+    static   String [] Deck=new String[52];
     static ArrayList<Integer> listArray=new ArrayList();
     public static void main(String[] args)
     {
@@ -23,31 +23,52 @@ public class decOfCards {
     public static void getCombination()
     {
 
-        int c=0;
+        int c=0,z=0,index;
 
-       for (int i=0;i< suit.length;i++)
+       for (int i=0;i<suit.length;i++)
        {
-           for (int j=0;j<rank.length;j++)
+           c=0;
+           for (int j=0;c<rank.length;j++)
            {
-                int index=ra.nextInt();
-                index=index%13;
-                if(arrayPresentOrNot(i,Math.abs(index)))
-                {
-                    System.out.println(""+suit[i]+" "+rank[Math.abs(index)]);
+
+               index=ra.nextInt(52);
+//               System.out.println("Index= "+index);
+               index=index%13;
+                boolean flag=arrayPresentOrNot(suit[i]+rank[Math.abs(index)],z);
+              if(flag)
+              {
+                System.out.println(""+suit[i]+" "+rank[Math.abs(index)]);
+                z++;
+                c++;
                 }
+
            }
+
        }
-    }
-    public static boolean arrayPresentOrNot(int i,int value)
+    for (int i=0;i<Deck.length;i++)
     {
-            if(listArray.contains(i+value))
-            {
-            return false;
+        System.out.println("item = "+Deck[i]);
+    }
+    }
+    public static boolean arrayPresentOrNot(String i,int c) {
+
+        boolean found = true;
+        if ( c == 0) {
+            Deck[c] = i;
+            found = true;
+        } else {
+            for (int j = 0; j<c; j++) {
+                if (Deck[j].equals(i)) {
+                    found = false;
+                    break;
+                }
             }
-            else
-            {
-                listArray.add(i+value);
-                return true;
-            }
+
+        }
+        if(found)
+        {
+            Deck[c]=i;
+        }
+        return found;
     }
 }
