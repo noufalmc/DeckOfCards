63cb494a64e05d146614b47c96c02381be8bdfe4
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-11 08:17:29 +0530

    Added:welcome mesaaage

---
+++ src/corejava/decOfCards.java
@@ -0,0 +1,16 @@
+/*
+* Author : NOUFAL MC
+* Date   : 11/01/2022
+* Dec of cards game
+*
+*
+*
+* */
+
+package corejava;
+public class decOfCards {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Decofcards Game");
+    }
+}
